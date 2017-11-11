191a51cd51dfb8da8dabc23095ef23c94b13b526
Joshua Wu <joshuawu15@gmail.com>
2017-11-11 11:46:23 -0600

    Added login and additional info pages

---
+++ js/additional_info.js
@@ -0,0 +1,16 @@
+$(document).ready(function() { 
+
+	document.getElementById("title_button_id").disabled = true;
+
+	$('#register_button_id').on('click',function(){ 
+		
+			$("#register_button_id").css("background-color","#a09486");
+			$("#login_button_id").css("background-color","#ABA194");
+			$("#login_form_content").hide();
+			$("#register_form_content").fadeIn(500);
+			
+			$("#form_submit_login").hide();
+			$("#form_submit_register").fadeIn(500);
+		});
+		
+});
---
+++ js/welcome_page.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function() { 
+
+	$("#login_form_content").hide();
+	$("#form_submit_login").hide();
+	
+	$('.btn').on('click', function () {
+		$('.form').addClass('form--no');
+		
+		window.location.href = "additional_info.html";
+	});
+	$('#register_button_id').on('click',function(){ 
+	
+		$("#register_button_id").css("background-color","#a09486");
+		$("#login_button_id").css("background-color","#ABA194");
+		$("#login_form_content").hide();
+		$("#register_form_content").fadeIn(500);
+		
+		$("#form_submit_login").hide();
+		$("#form_submit_register").fadeIn(500);
+	});
+	$('#login_button_id').on('click',function(){ 
+	
+		$('#login_button_id').css("background-color","#a09486");
+		$('#register_button_id').css("background-color","#ABA194");
+		$("#register_form_content").hide();
+		$("#login_form_content").fadeIn(500);
+		
+		$("#form_submit_register").hide();
+		$("#form_submit_login").fadeIn(500);
+	});
+
+	// For form transitions on button click
+
+});
